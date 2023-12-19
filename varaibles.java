bbb38c8aa277f1946b1e00bed0a8ffab21017357
Pradeep Kumar (pradeku8) <pradeku8@cisco.com>
2023-12-19 08:34:02 +0530

    Variables

---
+++ varaibles.java
@@ -0,0 +1,12 @@
+class Variables {
+    
+    public static void main (String[] args) {
+        int age = 30;
+        short height = 6;
+        double years = 200;
+        String f_name = "pradeep", l_name="kumar";
+        System.out.println(f_name + l_name);
+        System.out.println("Age is : " + age);
+        System.out.println("Height is: " + height);
+    }
+}
